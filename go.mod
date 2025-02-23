e5f568113374721d2eb2d3ecd0f881f08da499e1
Tillman Mosley III <tmosleyiii@gmail.com>
2025-02-23 05:14:22 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/withObsrvr/pluginapi
+
+go 1.23.4
---
+++ pluginapi.go
@@ -0,0 +1,65 @@
+// github.com/withObsrvr/pluginapi/pluginapi.go
+package pluginapi
+
+import (
+	"context"
+	"time"
+)
+
+// PluginType defines the category of a plugin.
+type PluginType int
+
+const (
+	SourcePlugin PluginType = iota
+	ProcessorPlugin
+	ConsumerPlugin
+)
+
+// Plugin is the basic interface that every plugin must implement.
+type Plugin interface {
+	// Name returns the plugin's unique name.
+	Name() string
+	// Version returns the plugin's version.
+	Version() string
+	// Type returns the category of the plugin.
+	Type() PluginType
+	// Initialize configures the plugin with a map of settings.
+	Initialize(config map[string]interface{}) error
+}
+
+// Source is an extension of Plugin that ingests data.
+type Source interface {
+	Plugin
+	// Start begins data ingestion.
+	Start(ctx context.Context) error
+	// Stop halts ingestion.
+	Stop() error
+	// Subscribe allows downstream processors to be added.
+	Subscribe(proc Processor)
+}
+
+// Processor is an extension of Plugin that transforms data.
+type Processor interface {
+	Plugin
+	// Process transforms a message.
+	Process(ctx context.Context, msg Message) error
+}
+
+// Consumer is an extension of Plugin that consumes data.
+type Consumer interface {
+	Plugin
+	// Process handles a message (e.g. storing it).
+	Process(ctx context.Context, msg Message) error
+	// Close cleans up any resources used by the consumer.
+	Close() error
+}
+
+// Message is the unified data structure that flows between plugins.
+type Message struct {
+	// Payload holds the primary data (often as []byte).
+	Payload interface{}
+	// Metadata is an optional map with additional info.
+	Metadata map[string]interface{}
+	// Timestamp indicates when the message was created.
+	Timestamp time.Time
+}
